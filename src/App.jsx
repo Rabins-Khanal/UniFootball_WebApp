4179e19ad2bd46f6c9999713870b294a6490518b
Rabins-Khanal <sjlkhanal@gmail.com>
2025-03-01 19:48:42 +0545

    routes fixed

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import CreatePost from './pages/CreatePost';
 import Dashboard from './pages/Dashboard';
 import Home from './pages/Home';
 import PostPage from './pages/PostPage';
+import Search from './pages/Search';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import UpdatePost from './pages/UpdatePost';
@@ -25,7 +26,7 @@ export default function App() {
         <Route path='/contact' element={<Contact />} />
         <Route path='/sign-in' element={<SignIn />} />
         <Route path='/sign-up' element={<SignUp />} />
-
+        <Route path='/search' element={<Search />} />
         <Route element={<PrivateRoute />}>
           <Route path='/dashboard' element={<Dashboard />} />
         </Route>
